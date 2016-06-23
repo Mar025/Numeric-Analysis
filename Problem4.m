7b4c7e83026ba643460d082e0514ea04ce9c184b
Mario <rivera_calero@hotmail.com>
2016-06-23 17:49:56 -0600

    Update Problem4.m
    This is the correct solution.

--- Problem4.m
+++ Problem4.m
@@ -1,38 +1,47 @@
-function [volumen]= Problem4(n)
+function [volumen_estimado]= Problem4(n)
 %Problem4.m
 %Valor esperado del volumen de un tetraedro formado por cuatro puntos tomados
 %aleatoriamente dentro del tetraedro los cuales son (0,0,0), (0,1,0), (0,0,1)
 % y (1,0,0)
 %
 %Entrada
-%n : numero de puntos para ver si estan dentro del tetraedro.
+%n : tamaño de la muestra de los tetraedro.
 %
 %Salida
-%volumen_estimado : casos favorables entre casos posibles.
+%volumen_estimado : media de los volumenes de los tetraedros.
 %
 %
 %Declaración de la función:
 %volumen_estimado = Problem4(n)
 %
 %Ejemplo 1
-%Calcular el volumen esperado utilizando una muestra de 100 puntos
-%>>volumen = Problem4(1000)
-%volumen = 0.16000
+%Calcular el volumen esperado utilizando una muestra de 100 tetraedros
+%>>volumen = Problem4(100)
+%volumen =  0.0029508
 %
 %Autores:
 %Mario Salvador Rivera Calero
 %Jonathan Vladimir Ramírez Juárez
-%06/06/2016cases = 0;
-favorables = 0; %Contador de casos donde el punto esta dentro del tetraedro
-for k = 1:n
-	x = rand;
-	y = rand;
-	z = rand;
-	if x+y+z<=1 %Verificamos que los puntos esten dentro del tetraedro
-		favorables = favorables +1; %Sumamos uno al contador si sucede esto.
+%06/06/2016
+
+volumenes = zeros(1,n); %Vector que contendrá los volumenes de cada tetraedro
+for j = 1:n;
+	matriz = ones(4,4); %Matriz para guardar las coordenadas del tetraedro y calcular su volumen
+	k = 0; %Contador para los puntos que guardaremos en la matriz
+	while k<4 %Necesitamos obtener 4 puntos
+		x = rand;
+		y = rand;
+		z = rand;
+		if x+y+z<=1 %Verificamos que el punto esté dentro del tetraedro
+			vector = [x y z]; %Punto que está en el tetraedro
+			matriz(k+1,1:3) = vector; %Agregamos el punto a la matriz
+			k = k+1; %Sumamos 1 al contador de los puntos
+		end
 	end
+	volumen = (1/6)*abs(det(matriz)); %Calculamos el volumen del j-esimo tetraedro
+	volumenes(j) = volumen; %Guardamos el volumen en el vector que contendra todos los volumenes
 endfor
-volumen = favorables/n; %Casos favorables entre casos posibles.
+volumen_estimado = sum(volumenes)/n; %Es la media de los volumenes.
 
 %EndOfFuntion
 
@@ -41,4 +50,3 @@ volumen = favorables/n; %Casos favorables entre casos posibles.
 %! volumen = Problem4(n);
 %! volumen
 %
-
